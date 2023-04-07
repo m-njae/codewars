8209f99906ff869a826944ed034d86c585d9ac1e
Minjae Cho <minjae1.cho@torontomu.ca>
2023-04-07 13:36:45 -0400

    .

---
+++ 8kyu/double-char.js
@@ -0,0 +1,18 @@
+// DESCRIPTION:
+
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// Examples (Input -> Output):
+
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+// Good Luck!
+
+//Solution 
+
+function doubleChar(str) {
+    return str.split('')
+              .map(x => x + x)
+              .join('')
+  }
