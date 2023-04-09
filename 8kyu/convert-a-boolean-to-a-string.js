4a9b1bb26c6233b2a700c446af256814b61fbe67
Minjae Cho <minjae1.cho@torontomu.ca>
2023-04-09 13:38:15 -0400

    .

---
+++ 8kyu/convert-a-boolean-to-a-string.js
@@ -0,0 +1,11 @@
+// DESCRIPTION:
+
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+//Solution 
+
+function booleanToString(b){
+    return b ? 'true' : 'false'
+  }
