a1b04ecd1c2516a537512b3a22bd930e84c48238
Minjae Cho <minjae1.cho@torontomu.ca>
2023-06-08 11:28:46 -0400

    .

---
+++ 6kyu/count-characters-in-your-string.js
@@ -0,0 +1,16 @@
+// DESCRIPTION:
+
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+//Solution 
+
+function count(string) {
+  let collection = {};
+  string = string.split('')
+  for(i = 0; i < string.length; i++){
+    collection[string[i]] = (collection[string[i]]||0) + 1;
+  }
+  return collection
+}
